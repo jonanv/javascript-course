c2bb4f3e241e7d0d0bbe171c2273713c1b3adf62
jonanv <jonan-vargas23@hotmail.com>
2023-09-06 11:54:43 -0500

    :sparkles: (console.table): Se crea console.table y estilos para el contenido de console.log

--- 01-fundamentals/app.js
+++ 01-fundamentals/app.js
@@ -1,1 +1,10 @@
-console.log('Hola mundo');
+// console.log('Hola mundo');
+
+let a = 10,
+    b = 20,
+    c = 'Hola',
+    d = 'Spiderman',
+    x = a + b;
+
+console.log('%c Mis variables', 'color: blue; font-weight: bold;');
+console.table({ a, b, c, d, x });
