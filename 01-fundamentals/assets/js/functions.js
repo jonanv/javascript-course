48967e13bd777099e89935aa5aec6237ee56f7a6
jonanv <jonan-vargas23@hotmail.com>
2025-04-02 16:55:42 -0500

    :sparkles: (01-fundamentals): Add arguments in functions

--- 01-fundamentals/assets/js/functions.js
+++ 01-fundamentals/assets/js/functions.js
@@ -29,8 +29,41 @@ const saludarFlecha = (nombre) => {
     console.log(`Hola, ${nombre}`);
 }
 
+function saludar5(nombre) {
+    console.log(arguments);
+    console.log(`Hola, ${nombre}`);
+    return 10;
+
+    console.log('Esto no se ejecuta nunca porque está después de un return');
+}
+
+function sumar(a, b) {
+    return a + b;
+}
+
+// Definición de una función flecha (arrow function) para sumar
+// Esta función es una versión más concisa de las funciones tradicionales
+// const sumar2 = (a, b) => {
+//     return a + b;
+// }
+const sumar2 = (a, b) => a + b; // Versión más corta de la función anterior
+
+function getAleatorio() {
+    return Math.random();
+}
+
+const getAleatorio2 = () => Math.random(); // Versión más corta de la función anterior
+
 saludar();
 saludar2();
 saludar3("Juan");
 saludar4("Juan", 40, true, 'Colombia');
 saludarFlecha("Juan");
+let retornoDeSaludar = saludar5("Juan", 40, true, 'Colombia');
+console.log({retornoDeSaludar}); // 10
+
+
+console.log(sumar(5, 10)); // 15
+console.log(sumar2(5, 10)); // 15
+console.log(getAleatorio()); // Número aleatorio entre 0 y 1
+console.log(getAleatorio2()); // Número aleatorio entre 0 y 1
