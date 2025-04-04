e88585987a9acb45f6cac6c33ad29c45b490f53e
jonanv <jonan-vargas23@hotmail.com>
2025-04-04 17:43:35 -0500

    :sparkles: (02-blackjack): Add methods btnStop and computerTurn

--- 02-blackjack/assets/js/game.js
+++ 02-blackjack/assets/js/game.js
@@ -14,7 +14,7 @@ const btnNewGame = document.querySelector("#btnNewGame");
 const btnGiveCard = document.querySelector("#btnGiveCard");
 const btnStop = document.querySelector("#btnStop");
 
-const playerPointsHTML = document.querySelectorAll("small");
+const smalls = document.querySelectorAll("small");
 
 const playerCards = document.querySelector("#player-cards");
 const computerCards = document.querySelector("#computer-cards");
@@ -61,14 +61,33 @@ const valueCard = (card) => {
 
 // valueCard(giveCard());
 
+// Turno de la computadora
+const computerTurn = (minPoints) => {
+    do {
+        const card = giveCard();
+        computerPoints += valueCard(card);
+
+        smalls[1].innerText = computerPoints;
+
+        const imgCard = document.createElement("img");
+        imgCard.src = `assets/imgs/cartas/${card}.png`;
+        imgCard.classList.add("custom-card");
+        computerCards.append(imgCard);
+
+        if (minPoints > 21) {
+            break;
+        }
+
+    } while ((computerPoints < minPoints) && (minPoints <= 21));
+    // console.log(computerPoints);
+}
+
 // Eventos
 btnGiveCard.addEventListener("click", () => {
     const card = giveCard();
-    // console.log(card);
+    playerPoints += valueCard(card);
 
-    const points = valueCard(card);
-    playerPoints += points;
-    playerPointsHTML[0].innerText = playerPoints;
+    smalls[0].innerText = playerPoints;
 
     // playerCards.innerHTML = `<img class="custom-card" src="assets/imgs/cartas/${card}.png">`;
     const imgCard = document.createElement("img");
@@ -80,10 +99,28 @@ btnGiveCard.addEventListener("click", () => {
         console.warn("Perdiste");
         btnGiveCard.disabled = true;
         btnStop.disabled = true;
+        computerTurn(playerPoints);
     } else if (playerPoints === 21) {
         console.warn("Ganaste");
         btnGiveCard.disabled = true;
         btnStop.disabled = true;
+        computerTurn(playerPoints);
     }
 });
 
+btnStop.addEventListener("click", () => {
+    btnGiveCard.disabled = true;
+    btnStop.disabled = true;
+
+    computerTurn(playerPoints);
+
+    if (computerPoints > 21) {
+        console.warn("Ganaste");
+    } else if (computerPoints === playerPoints) {
+        console.warn("Empate");
+    } else if (computerPoints > playerPoints) {
+        console.warn("Perdiste");
+    } else {
+        console.warn("Ganaste");
+    }
+});
