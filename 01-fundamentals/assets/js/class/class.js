88c85cb580be03a922eeff7f8f606ae70a26ed70
jonanv <jonan-vargas23@hotmail.com>
2025-04-09 18:22:20 -0500

    :sparkles: (01-fundamentals): Add properties, gets and static methods

--- 01-fundamentals/assets/js/class/class.js
+++ 01-fundamentals/assets/js/class/class.js
@@ -2,6 +2,18 @@
 // Forma moderna de crear objetos en javascript
 class Person {
     // 'use strict'; // Modo estricto para evitar errores comunes ya viene activado por defecto
+
+    // Propiedades estáticas
+    static _count = 0;
+    static get count() {
+        return Person._count + ' instancias';
+    }
+
+    static message() {
+        console.log(this.name); // undefined
+        console.log('Hola mundo desde un método estático');
+    }
+
     // Propiedades
     name = '';
     code = '';
@@ -13,6 +25,8 @@ class Person {
         this.name = name;
         this.code = code;
         this.sentence = sentence;
+
+        Person._count++;
     }
 
     // Setters
@@ -33,8 +47,6 @@ class Person {
     mySentence() {
     console.log(`${ this.code } dice: ${ this.sentence }`);
     }
-
-
 }
 
 const spiderman = new Person('Peter Parker', 'Spiderman', 'Soy tu amigo y vecino Spiderman');
@@ -47,3 +59,7 @@ console.log(spiderman.getFood); // Imprime la comida en mayúsculas
 
 // ironman.whoAmI();
 // ironman.mySentence();
+
+// Person._count = 0; // Se puede acceder a la propiedad estática de la clase directamente
+console.log('Conteo estático:', Person.count);
+Person.message();
