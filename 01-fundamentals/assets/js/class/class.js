8627e021fc9fb53cfad18a372b03e17fa9084919
jonanv <jonan-vargas23@hotmail.com>
2025-04-09 18:03:35 -0500

    :sparkles: (01-fundamentals): Add setters and getters

--- 01-fundamentals/assets/js/class/class.js
+++ 01-fundamentals/assets/js/class/class.js
@@ -4,20 +4,46 @@ class Person {
     // 'use strict'; // Modo estricto para evitar errores comunes ya viene activado por defecto
     // Propiedades
     name = '';
-    age = 0;
+    code = '';
+    sentence = '';
+    food = '';
 
     // Constructor
-    constructor(name, age) {
+    constructor(name = 'Sin nombre', code = 'Sin código', sentence = 'Sin frase') {
         this.name = name;
-        this.age = age;
+        this.code = code;
+        this.sentence = sentence;
+    }
+
+    // Setters
+    set setFood(food) {
+        this.food = food;
+    }
+
+    // Getters
+    get getFood() {
+        return this.food.toUpperCase(); // Devuelve la comida en mayúsculas
     }
 
     // Métodos
-    print() {
-        console.log(`Nombre: ${ this.name } - edad: ${ this.age }`);
+    whoAmI() {
+        console.log(`Soy ${ this.name } y mi identidad es ${ this.code }`);
     }
+
+    mySentence() {
+    console.log(`${ this.code } dice: ${ this.sentence }`);
+    }
+
+
 }
 
-const maria = new Person("Maria", 22);
-console.log(maria);
-maria.print();
+const spiderman = new Person('Peter Parker', 'Spiderman', 'Soy tu amigo y vecino Spiderman');
+const ironman = new Person('Tony Stark', 'Ironman', 'Yo soy Ironman');
+
+spiderman.whoAmI();
+spiderman.mySentence();
+spiderman.setFood = 'Pizza'; // Setea la comida
+console.log(spiderman.getFood); // Imprime la comida en mayúsculas
+
+// ironman.whoAmI();
+// ironman.mySentence();
