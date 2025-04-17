cee2869129bf17cdf1e9a10c39d7ec8ce3e6de32
jonanv <jonan-vargas23@hotmail.com>
2025-04-17 13:53:36 -0500

    :art: (05-todo-app): Improve code about li element

--- 05-todo-app/src/todos/use-cases/create-todo-html.js
+++ 05-todo-app/src/todos/use-cases/create-todo-html.js
@@ -6,18 +6,17 @@
 export const createTodoHtml = (todo) => {
     if (!todo) throw new Error('Todo is required');
 
-    const li = document.createElement('li');
-    li.setAttribute('data-id', todo.id);
-    li.className = 'todo-item';
-    li.innerHTML = `
-        <li class="${ todo.done }" data-id="abc">
-            <div class="view">
-                <input class="toggle" type="checkbox" checked>
-                <label>${ todo.description }</label>
-                <button class="destroy"></button>
-            </div>
-            <input class="edit" value="Create a TodoMVC template">
-        </li>
+    const html = `
+        <div class="view">
+            <input class="toggle" type="checkbox" ${ todo.done ? 'checked' : '' }>
+            <label>${ todo.description }</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
     `;
-    return li;
+
+    const liElement = document.createElement('li');
+    liElement.setAttribute('data-id', todo.id);
+    liElement.innerHTML = html;
+    return liElement;
 }
