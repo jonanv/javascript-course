9b6a72919ff7f1051cb9015ae1bc28c3383a0edb
jonanv <jonan-vargas23@hotmail.com>
2025-04-17 14:10:04 -0500

    :art: (05-todo-app): Implement destructuring over todo object

--- 05-todo-app/src/todos/use-cases/create-todo-html.js
+++ 05-todo-app/src/todos/use-cases/create-todo-html.js
@@ -6,17 +6,19 @@
 export const createTodoHtml = (todo) => {
     if (!todo) throw new Error('Todo is required');
 
+    const { id, description, done, createAt } = todo;
+
     const html = `
         <div class="view">
-            <input class="toggle" type="checkbox" ${ todo.done ? 'checked' : '' }>
-            <label>${ todo.description }</label>
+            <input class="toggle" type="checkbox" ${ done ? 'checked' : '' }>
+            <label>${ description }</label>
             <button class="destroy"></button>
         </div>
         <input class="edit" value="Create a TodoMVC template">
     `;
 
     const liElement = document.createElement('li');
-    liElement.setAttribute('data-id', todo.id);
+    liElement.setAttribute('data-id', id);
     liElement.innerHTML = html;
     return liElement;
 }
