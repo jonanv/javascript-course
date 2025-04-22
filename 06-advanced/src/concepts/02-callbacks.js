c9aaef275402baf65d770fdabcf945f780b55c27
jonanv <jonan-vargas23@hotmail.com>
2025-04-22 14:07:22 -0500

    :sparkles: (06-advanced): Add manage of callbacks

--- 06-advanced/src/concepts/02-callbacks.js
+++ 06-advanced/src/concepts/02-callbacks.js
@@ -7,7 +7,14 @@ import { heroes } from '../data/heroes';
 export const callbacksComponent = (element) => {
     
     const id = '5d86371f25a058e5b1c8a65e';
-    findHeroe(id, (heroe) => {
+    findHeroe(id, (error, heroe) => {
+        // element.innerHTML = heroe?.name || 'No se ha encontrado el heroe';
+        // element.innerHTML = heroe?.name || error;
+
+        if (error) {
+            element.innerHTML = error;
+            return;
+        }
         element.innerHTML = heroe.name;
     });
 }
@@ -15,10 +22,20 @@ export const callbacksComponent = (element) => {
 /**
  * @description Funcion que busca un heroe por su id
  * @param {String} id Id del heroe a buscar
- * @param {(hero: Object) => void} callback Callback que se ejecuta al encontrar el heroe
+ * @param {(error: String | null, hero: Object) => void} callback Callback que se ejecuta al encontrar el heroe
  */
 const findHeroe = (id, callback) => {
+    if (!id) throw new Error('No se ha proporcionado un id');
+    if (typeof callback !== 'function') throw new Error('El callback no es una funcion');
+
     const heroe = heroes.find(heroe => heroe.id === id);
 
-    callback(heroe);
+    // if (!heroe) callback(`No se ha encontrado el heroe con el id ${ id }`, null);
+    // else callback(null, heroe);
+
+    if (!heroe) {
+        callback(`No se ha encontrado el heroe con el id ${ id }`, null);
+        return;
+    }
+    callback(null, heroe);
 }
