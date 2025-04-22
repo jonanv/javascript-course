db9f7ac0c9c976a6084ae20911d9e05535cb002b
jonanv <jonan-vargas23@hotmail.com>
2025-04-22 14:25:53 -0500

    :bug: (06-advanced): Fixed to variables and name methods

--- 06-advanced/src/concepts/02-callbacks.js
+++ 06-advanced/src/concepts/02-callbacks.js
@@ -8,21 +8,21 @@ export const callbacksComponent = (element) => {
     
     const id1 = '5d86371f25a058e5b1c8a65e';
     const id2 = '5d86371f233c9f2425f16916';
-    findHeroe(id1, (error, heroe1) => {
-        // element.innerHTML = heroe?.name || 'No se ha encontrado el heroe';
-        // element.innerHTML = heroe?.name || error;
+    findHero(id1, (error, hero1) => {
+        // element.innerHTML = hero?.name || 'No se ha encontrado el heroe';
+        // element.innerHTML = hero?.name || error;
 
         if (error) {
             element.innerHTML = error;
             return;
         }
 
-        findHeroe(id2, (error, heroe2) => {
+        findHero(id2, (error, hero2) => {
             if (error) {
                 element.innerHTML = error;
                 return;
             }
-            element.innerHTML = `${ heroe1.name } / ${ heroe2.name }`;
+            element.innerHTML = `${ hero1.name } / ${ hero2.name }`;
         });
     });
 }
@@ -32,18 +32,18 @@ export const callbacksComponent = (element) => {
  * @param {String} id Id del heroe a buscar
  * @param {(error: String | null, hero: Object) => void} callback Callback que se ejecuta al encontrar el heroe
  */
-const findHeroe = (id, callback) => {
+const findHero = (id, callback) => {
     if (!id) throw new Error('No se ha proporcionado un id');
     if (typeof callback !== 'function') throw new Error('El callback no es una funcion');
 
-    const heroe = heroes.find(heroe => heroe.id === id);
+    const hero = heroes.find(hero => hero.id === id);
 
     // if (!heroe) callback(`No se ha encontrado el heroe con el id ${ id }`, null);
     // else callback(null, heroe);
 
-    if (!heroe) {
+    if (!hero) {
         callback(`No se ha encontrado el heroe con el id ${ id }`, null);
         return;
     }
-    callback(null, heroe);
+    callback(null, hero);
 }
