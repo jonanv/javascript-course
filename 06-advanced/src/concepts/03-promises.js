2bea89a37a7e42ab8cac39e8858ebac0242cb96d
jonanv <jonan-vargas23@hotmail.com>
2025-04-22 14:58:57 -0500

    :recycle: (06-advanced): Refactor code into promises

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -6,15 +6,23 @@ import { heroes } from "../data/heroes";
  */
 export const promisesComponent = (element) => {
     
-    const id = '5d86371f2343e37870b91ef1';
+    const renderHero = (hero) => {
+        element.innerHTML = hero.name;
+    }
 
-    findHero(id)
-        .then(hero => {
-            element.innerHTML = hero.name;
-        })
-        .catch(error => {
-            element.innerHTML = error;
-        });
+    const renderError = (error) => {
+        element.innerHTML = `
+            <h1>Error</h1>
+            <p>${ error }</p>
+        `;
+    }
+
+    const id1 = '5d86371f2343e37870b91ef1';
+    const id2 = '5d86371f25a058e5b1c8a65e';
+
+    findHero(id1)
+        .then(renderHero)
+        .catch(renderError);
 }
 
 /**
