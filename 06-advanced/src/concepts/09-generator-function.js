98d6d118c8a6cd6114248da14726235fdd5e74db
jonanv <jonan-vargas23@hotmail.com>
2025-04-24 16:39:16 -0500

    :art: (06-advanced): Add example about generator function

--- 06-advanced/src/concepts/09-generator-function.js
+++ 06-advanced/src/concepts/09-generator-function.js
@@ -4,15 +4,38 @@
  */
 export const generatorFunctionsComponent = (element) => {
     
-    const myGenerator  = myFirstGeneratorFunction();
+    // const myGenerator  = myFirstGeneratorFunction();
 
-    console.log(myGenerator.next());    // { value: 'Primer valor', done: false }
-    console.log(myGenerator.next());    // { value: 'Segundo valor', done: false }
-    console.log(myGenerator.next());    // { value: 'Tercer valor', done: false }
-    console.log(myGenerator.next());    // { value: 'Cuarto valor', done: false }
-    console.log(myGenerator.next());    // { value: 'Ya no hya valores', done: true }
-    console.log(myGenerator.next());    // { value: undefined, done: true }
+    // console.log(myGenerator.next());    // { value: 'Primer valor', done: false }
+    // console.log(myGenerator.next());    // { value: 'Segundo valor', done: false }
+    // console.log(myGenerator.next());    // { value: 'Tercer valor', done: false }
+    // console.log(myGenerator.next());    // { value: 'Cuarto valor', done: false }
+    // console.log(myGenerator.next());    // { value: 'Ya no hya valores', done: true }
+    // console.log(myGenerator.next());    // { value: undefined, done: true }
 
+    const idGenerator = idIterator();
+    // console.log(idGenerator.next());   // { value: 1, done: false }
+    // console.log(idGenerator.next());   // { value: 2, done: false }
+    // console.log(idGenerator.next());   // { value: 3, done: false }
+
+    const button = document.createElement('button');
+    button.innerText = 'Click me!';
+    element.append(button);
+
+    const renderButton = () => {
+        const { value } = idGenerator.next();
+        button.innerHTML = `Click ${ value }`;
+    }
+
+    button.addEventListener('click', renderButton);
+
+}
+
+function* idIterator() {
+    let currentId = 0;
+    while (true) {
+        yield ++currentId;
+    }
 }
 
 function* myFirstGeneratorFunction() {
