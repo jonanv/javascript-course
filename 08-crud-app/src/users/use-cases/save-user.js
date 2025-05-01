122070f24e41b2a86a5370cd7e25c48a5118e2a5
jonanv <jonan-vargas23@hotmail.com>
2025-05-01 15:18:46 -0500

    :sparkles: (08-crud-app): Create uses case save user

---
+++ 08-crud-app/src/users/use-cases/save-user.js
@@ -0,0 +1,39 @@
+import { User } from "../models/user";
+
+/**
+ * @description Guarda un nuevo usuario en la API
+ * @param {Like<User>} userLike 
+ */
+export const saveUser = async(userLike) => {
+    const user = new User(userLike);
+
+    // TODO: Aqui falta un mapper
+
+    if (user.id) {
+        throw new Error('No implementado');
+        return;
+    }
+
+    const updatedUser = await createUser(user);
+    return updatedUser;
+}
+
+/**
+ * @description Crea un nuevo usuario en la API
+ * @param {Like<User>} user 
+ * @returns {Promise<User>} 
+ */
+export const createuser = async(user) => {
+    const url = `${ import.meta.env.VITE_API_URL }/users`;
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    });
+
+    const newUser = await response.json();
+    console.log({ newUser });
+    return newUser;
+}
