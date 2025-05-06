e560758ba0dfd5d67fb8a1805d02180b875ba06f
jonanv <jonan-vargas23@hotmail.com>
2025-05-06 17:38:48 -0500

    :sparkles: (09-esnext): Add about structuredClone

--- 09-esnext/src/01-arrays/array-with.js
+++ 09-esnext/src/01-arrays/array-with.js
@@ -1,1 +1,33 @@
-console.log('Array with');
+const superHeroes = [
+    {
+        id: 1,
+        name: 'Batman',
+    },
+    {
+        id: 2,
+        name: 'Superman',
+    },
+    {
+        id: 3,
+        name: 'Wonder Woman',
+    },
+    {
+        id: 4,
+        name: 'Aquaman',
+    },
+];
+
+// Copy
+const superHeroesCopy = [...superHeroes];
+superHeroesCopy[0].name = 'Bruce Wayne';
+
+const superHeroesCopy2 = JSON.parse(JSON.stringify(superHeroes));
+superHeroesCopy2[0].name = 'Bruce W';
+
+const superHeroesCopy3 = structuredClone(superHeroes);
+superHeroesCopy3[0].name = 'Batman';
+
+console.table(superHeroes);
+console.table(superHeroesCopy);
+console.table(superHeroesCopy2);
+console.table(superHeroesCopy3);
